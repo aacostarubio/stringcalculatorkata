d59aed972638a31cf3b90f0e07269fb6973b5782
Alejandro Acosta-Rubio <mastermind@Alejandros-MacBook-Air.local>
2013-03-11 14:27:49 -0500

    it was getting too repeatative, so Added map method, and inject method.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,10 +4,8 @@ class StringCalculator
   return 0 if string.empty?
   return string.to_i unless string.include?(",")
   stringing_numbers = string.split(",")
-  return stringing_numbers[0].to_i + stringing_numbers[1].to_i if stringing_numbers.size == 2
-  return stringing_numbers[0].to_i + stringing_numbers[1].to_i + stringing_numbers[2].to_i if stringing_numbers.size == 3
-  stringing_numbers[0].to_i + stringing_numbers[1].to_i + stringing_numbers[2].to_i + stringing_numbers[3].to_i
-
+  numbers = stringing_numbers.map { |n| n.to_i}
+  numbers.inject(:+)
  end
 
 

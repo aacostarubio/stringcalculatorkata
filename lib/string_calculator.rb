b33a92b1a2e4800191e0ad9272fddc91630ccac6
Alejandro Acosta-Rubio <mastermind@Alejandros-MacBook-Air.local>
2013-03-11 14:50:15 -0500

    I love regexp. End of story.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,13 +2,15 @@ class StringCalculator
 
  def add(string)
   return 0 if string.empty?
+  delimeters = [",", "\n"]
+
   if string.include?("\n")
-  	stringing_numbers = string.split("\n")
+  	stringing_numbers = string.split(Regexp.union(delimeters))
   	numbers = stringing_numbers.map { |n| n.to_i}
   	return numbers.inject(:+)
   end
 
-  stringing_numbers = string.split(",")
+  stringing_numbers = string.split(Regexp.union(delimeters))
   numbers = stringing_numbers.map { |n| n.to_i}
   numbers.inject(:+)
  end

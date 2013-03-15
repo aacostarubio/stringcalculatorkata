bb0188b1252712841b4c8b26211762b547943d36
Alejandro Acosta-Rubio <mastermind@Alejandros-MacBook-Air.local>
2013-03-15 15:05:05 -0500

    refactored to different methods

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,19 +1,15 @@
 class StringCalculator
+  attr_accessor :string
 
  def add(string)
   return 0 if string.empty?
-  delimeters = [",", "\n"]
-
-  if string.include?("\n")
-  	stringing_numbers = string.split(Regexp.union(delimeters))
-  	numbers = stringing_numbers.map { |n| n.to_i}
-  	return numbers.inject(:+)
-  end
-  
-  stringing_numbers = string.split(Regexp.union(delimeters))
-  numbers = stringing_numbers.map { |n| n.to_i}
-  numbers.inject(:+)
+  @string = string
+  stringing_numbers.map { |n| n.to_i}.inject(:+)
  end
 
+ def stringing_numbers
+  delimeters = [",", "\n"]
+  @string.split(Regexp.union(delimeters))  
+ end
 
 end
